6211663a4c1c7babdb58fc377c940d76a8c0caea
Yassine <yassinesania@gmail.com>
2022-05-31 18:37:30 +0200

    Set Aurora GraphQLClient

--- data/index.js
+++ data/index.js
@@ -3,7 +3,7 @@ import { GraphQLClient } from 'graphql-request' // GraphQL request client
 // Create client
 const client = new GraphQLClient(
     // Zora Aurora subgraph
-    'https://api.thegraph.com/subgraphs/name/curaos/zora-v1-subgraph'
+    'https://api.thegraph.com/subgraphs/name/curaos/zora-on-aurora'
 )
 
 // Export client
