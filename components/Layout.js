119cf1982424258fb93b742c17ecc708030e9447
Yassine <yassinesania@gmail.com>
2022-06-01 16:25:08 +0200

    Update naming

--- components/Layout.js
+++ components/Layout.js
@@ -22,8 +22,8 @@ function Meta({ isProfile }) {
     return (
         <Head>
             {/* Primary Meta Tags */}
-            <title>Zora Gallery</title>
-            <meta name="title" content="Zora Gallery" />
+            <title>Cura Gallery</title>
+            <meta name="title" content="Cura Gallery" />
             <meta
                 name="description"
                 content="Open protocols demand open access. Community-operated interface to ZoraOS."
@@ -31,8 +31,8 @@ function Meta({ isProfile }) {
 
             {/* Open Graph / Facebook */}
             <meta property="og:type" content="website" />
-            <meta property="og:url" content="https://zora.gallery/" />
-            <meta property="og:title" content="Zora Gallery" />
+            <meta property="og:url" content="https://cura.run/" />
+            <meta property="og:title" content="Cura Gallery" />
             <meta
                 property="og:description"
                 content="Open protocols demand open access. Community-operated interface to ZoraOS."
@@ -40,8 +40,8 @@ function Meta({ isProfile }) {
 
             {/* Twitter */}
             <meta property="twitter:card" content="summary_large_image" />
-            <meta property="twitter:url" content="https://zora.gallery/" />
-            <meta property="twitter:title" content="Zora Gallery" />
+            <meta property="twitter:url" content="https://cura.run/" />
+            <meta property="twitter:title" content="Cura Gallery" />
             <meta
                 property="twitter:description"
                 content="Open protocols demand open access. Community-operated interface to ZoraOS."
@@ -52,11 +52,11 @@ function Meta({ isProfile }) {
                 <>
                     <meta
                         property="og:image"
-                        content="https://zora.gallery/meta.png"
+                        content="https://cura.run/meta.png"
                     />
                     <meta
                         property="twitter:image"
-                        content="https://zora.gallery/meta.png"
+                        content="https://cura.run/meta.png"
                     />
                 </>
             ) : null}
